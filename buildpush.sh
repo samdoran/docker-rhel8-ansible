dd914384e095daa98fb6245fc6e10cac9c2f85b8
Sam Doran <sdoran@redhat.com>
2019-07-09 12:39:29 -0400

    Update push script

--- buildpush.sh
+++ buildpush.sh
@@ -5,5 +5,5 @@ set -e
 echo "Building container image"
 docker build -t quay.io/samdoran/rhel8-ansible --build-arg RHSM_USERNAME=$RHSM_USERNAME --build-arg RHSM_PASSWORD=$RHSM_PASSWORD --build-arg RHSM_POOL_ID=$RHSM_POOL_ID .
 
-# echo "Pushing to Quay"
-# docker push quay.io/samdoran/rhel8-ansible
+echo "Pushing to Quay"
+docker push quay.io/samdoran/rhel8-ansible
