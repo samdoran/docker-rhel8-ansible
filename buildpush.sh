469eb40037aaa04bc3e4aef477dab74d29760509
Sam Doran <sdoran@redhat.com>
2020-06-23 16:13:48 -0400

    Commit message

--- buildpush.sh
+++ buildpush.sh
@@ -2,8 +2,21 @@
 
 set -e
 
-echo "Building container image"
-docker build -t quay.io/samdoran/rhel8-ansible --build-arg RHSM_USERNAME=$RHSM_USERNAME --build-arg RHSM_PASSWORD=$RHSM_PASSWORD --build-arg RHSM_POOL_ID=$RHSM_POOL_ID .
+function cleanup {
+    echo "Deleting secrets"
+    rm -f ./secrets
+}
+trap cleanup EXIT
 
-echo "Pushing to Quay"
-docker push quay.io/samdoran/rhel8-ansible
+echo "Creating secrets"
+# docker swarm init
+printf $RHSM_USERNAME | docker secret create rhsm_username -
+printf $RHSM_PASSWORD | docker secret create rhsm_password -
+printf $RHSM_POOL_ID | docker secret create rhsm_pool_id -
+
+echo "Building container image"
+DOCKER_BUILDKIT=1 docker build -t quay.io/samdoran/rhel8-ansible .
+
+
+# echo "Pushing to Quay"
+# docker push quay.io/samdoran/rhel8-ansible
